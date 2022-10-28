865e28bea8d59d70c564cda8ea0eb3dc6d5358fe
= <=>
2022-10-28 17:51:46 +0400

    Added enum and dto object

---
+++ main.js
@@ -0,0 +1,33 @@
+/**
+ * Enum for the gender
+ */
+const gender = {
+    male: 'male',
+    female: 'female'
+}
+
+/**
+ * Input object
+ */
+const dtoIn = {
+    count:50,
+    age : {
+        min: 19,
+        max: 35
+    }
+
+}
+
+/**
+ * Output object
+ */
+const dtoOut = {
+    
+
+}
+
+function main() {
+    dtoOut = new dtoOut()
+
+
+}
